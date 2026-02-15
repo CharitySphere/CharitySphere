24a3807eea3d599efaf4f62efe5ea35ec99c74ee
MidHunterX <midhunterx@gmail.com>
2026-02-15 06:28:09 +0530

    ✨ feat(mod_home): add multi step homepage

--- mod_home/urls.py
+++ mod_home/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="homepage"),
+    path("", views.home1, name="homepage"),
+    path("home2/", views.home2, name="homepage2"),
 ]
--- mod_home/views.py
+++ mod_home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'homepage.html')
+def home1(request):
+    return render(request, 'homepage1.html')
+
+def home2(request):
+    return render(request, 'homepage2.html')
